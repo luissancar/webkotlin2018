598c4d273970225065ecfb1970341ae2bfd85b9f
luissancar <chullo13250>
2018-09-21 11:10:32 +0200

    uno

--- app/src/main/java/com/example/luissancar/web/MainActivity.kt
+++ app/src/main/java/com/example/luissancar/web/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
-//
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
