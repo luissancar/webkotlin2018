a69568f944c2ff84c3d336f6367240ba11dc8e93
luissancar <chullo13250>
2018-09-21 11:06:49 +0200

    uno

---
+++ app/src/main/java/com/example/luissancar/web/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.luissancar.web
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        web.loadUrl("http://www.google.es")
+
+    }
+
+}
